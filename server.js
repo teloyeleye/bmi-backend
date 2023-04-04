bf6f7853d8442bcc7ebc62c9ac3182f9538848ef
Taye Oyeleye <teloyeleye@gmail.com>
2023-04-04 11:14:56 +0100

    setup

--- server.js
+++ server.js
@@ -10,8 +10,21 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); // req.body
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Max-Age", "1800");
+  res.setHeader("Access-Control-Allow-Headers", "content-type");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "PUT, POST, GET, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
 const PORT = process.env.PORT;
 
+// Database Connection
 dbConnect();
 
 app.listen({ port: PORT }, async () => {
